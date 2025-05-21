0c4a7abe2f25244995fc753991abf22a9555ebf7
saubten <saubten.mane@gmail.com>
2025-05-21 09:47:55 +0530

    Model added Connction String added Nuget Packages added EF

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        [Key] //DiaryEntryID and Id -> Don't need the [Key] Annotation
+        public int Id { get; set; }
+
+        [Required] //Makes it Non Nullable Field
+        public string Title { get; set; } = string.Empty;
+
+        //To handle a field which is set as Non Nullable 
+        //public string Title { get; set; } = string.Empty; 
+        //public required string Title { get; set; }  
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Now;
+
+    }
+}
