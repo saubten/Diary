775c226104adb1e33cc0e9c14d3c7b2b7bcf3511
saubten <saubten.mane@gmail.com>
2025-05-21 10:11:26 +0530

    EF Setup Database Setup

---
+++ DiaryApp/Data/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using DiaryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> context)  : base (context)
+        {
+            
+        }
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        //Steps to Add a Table using Entity Framework
+        //1. Create a Model (Class) for the Table (Eg. DiaryEntry)
+        //2. Add DB Set
+        //3. add-migration AddDiaryEntryTable
+        //4. update-database
+    }
+}
