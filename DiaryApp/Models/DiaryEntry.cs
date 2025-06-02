5d432f45624e05a11431fe956f4a3716c03f1d85
saubten <saubten.mane@gmail.com>
2025-06-02 23:31:50 +0530

    Validation on the Client side

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -7,17 +7,23 @@ namespace DiaryApp.Models
         [Key] //DiaryEntryID and Id -> Don't need the [Key] Annotation
         public int Id { get; set; }
 
-        [Required] //Makes it Non Nullable Field
+
+        [Required(ErrorMessage ="Bro you gotta enter Title")]
+        //Required - Makes it Non Nullable Field
+        //Error Message Takes care of the Validation of the Non Nullable Field
+        [StringLength(100,MinimumLength =3,ErrorMessage ="Bro Title must be between 3 and 100 length!!")]
+        //StringLength - Max Min length along with the Error Message is setup
         public string Title { get; set; } = string.Empty;
 
         //To handle a field which is set as Non Nullable 
         //public string Title { get; set; } = string.Empty; 
         //public required string Title { get; set; }  
 
-        [Required]
+        [Required(ErrorMessage = "Bro you forgot to enter Content")]//Makes it Non Nullable Field
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Bro Title must be between 3 and 100 length!!")]
         public string Content { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Lazy bastard just enter the Date")]//Makes it Non Nullable Field    
         public DateTime Created { get; set; } = DateTime.Now;
 
     }
